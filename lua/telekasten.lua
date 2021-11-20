2bc1a11672c4e2130cda83f14be72a263258d1e5
Rene Schallner <rene@rocks.ai>
2021-11-21 00:00:31 +0100

    it's coming together, update README

--- lua/telekasten.lua
+++ lua/telekasten.lua
@@ -70,7 +70,6 @@ end
 -- 
 find_daily_notes = function(opts)
     if (check_local_finder() == true) then 
-            print(zkcfg.dailies)
             builtin.find_files({
             prompt_title = "Find daily note",
             cwd = zkcfg.dailies,
@@ -113,6 +112,7 @@ follow_link = function(opts)
         prompt_title = "Follow link to note...",
         cwd = zkcfg.home,
         default_text = vim.fn.expand("<cword>"),
+        find_command = { zkcfg.daily_finder },
         entry_maker = zk_entry_maker,
    })
 end
@@ -123,130 +123,32 @@ end
 -- Select from notes
 -- 
 find_notes = function(opts)
-    opts = {} or opts
- 
-  local find_command = opts.find_command
-  local hidden = opts.hidden
-  local no_ignore = opts.no_ignore
-  local follow = opts.follow
-  local search_dirs = opts.search_dirs
+    builtin.find_files({
+        prompt_title = "Find notes by name",
+        cwd = zkcfg.home,
+        find_command = { zkcfg.daily_finder },
+        entry_maker = zk_entry_maker,
+   })
+end
 
-  if search_dirs then
-    for k, v in pairs(search_dirs) do
-      search_dirs[k] = vim.fn.expand(v)
-    end
-  end
-
-  if not find_command then
-    if 1 == vim.fn.executable "fd" then
-      find_command = { "fd", "--type", "f" }
-      if hidden then
-        table.insert(find_command, "--hidden")
-      end
-      if no_ignore then
-        table.insert(find_command, "--no-ignore")
-      end
-      if follow then
-        table.insert(find_command, "-L")
-      end
-      if search_dirs then
-        table.insert(find_command, ".")
-        for _, v in pairs(search_dirs) do
-          table.insert(find_command, v)
-        end
-      end
-    elseif 1 == vim.fn.executable "fdfind" then
-      find_command = { "fdfind", "--type", "f" }
-      if hidden then
-        table.insert(find_command, "--hidden")
-      end
-      if no_ignore then
-        table.insert(find_command, "--no-ignore")
-      end
-      if follow then
-        table.insert(find_command, "-L")
-      end
-      if search_dirs then
-        table.insert(find_command, ".")
-        for _, v in pairs(search_dirs) do
-          table.insert(find_command, v)
-        end
-      end
-    elseif 1 == vim.fn.executable "rg" then
-      find_command = { "rg", "--files" }
-      if hidden then
-        table.insert(find_command, "--hidden")
-      end
-      if no_ignore then
-        table.insert(find_command, "--no-ignore")
-      end
-      if follow then
-        table.insert(find_command, "-L")
-      end
-      if search_dirs then
-        for _, v in pairs(search_dirs) do
-          table.insert(find_command, v)
-        end
-      end
-    elseif 1 == vim.fn.executable "find" and vim.fn.has "win32" == 0 then
-      find_command = { "find", ".", "-type", "f" }
-      if not hidden then
-        table.insert(find_command, { "-not", "-path", "*/.*" })
-        find_command = flatten(find_command)
-      end
-      if no_ignore ~= nil then
-        log.warn "The `no_ignore` key is not available for the `find` command in `find_files`."
-      end
-      if follow then
-        table.insert(find_command, "-L")
-      end
-      if search_dirs then
-        table.remove(find_command, 2)
-        for _, v in pairs(search_dirs) do
-          table.insert(find_command, 2, v)
-        end
-      end
-    elseif 1 == vim.fn.executable "where" then
-      find_command = { "where", "/r", ".", "*" }
-      if hidden ~= nil then
-        log.warn "The `hidden` key is not available for the Windows `where` command in `find_files`."
-      end
-      if no_ignore ~= nil then
-        log.warn "The `no_ignore` key is not available for the Windows `where` command in `find_files`."
-      end
-      if follow ~= nil then
-        log.warn "The `follow` key is not available for the Windows `where` command in `find_files`."
-      end
-      if search_dirs ~= nil then
-        log.warn "The `search_dirs` key is not available for the Windows `where` command in `find_files`."
-      end
-    end
-  end
-
-  if not find_command then
-    print(
-      "You need to install either find, fd, or rg. "
-        .. "You can also submit a PR to add support for another file finder :)"
-    )
-    return
-  end
-
-  if opts.cwd then
-    opts.cwd = vim.fn.expand(opts.cwd)
-  end
-
-  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
-
-  pickers.new(opts, {
-    prompt_title = "Find Files",
-    finder = finders.new_oneshot_job(find_command, opts),
-    previewer = conf.file_previewer(opts),
-    sorter = conf.file_sorter(opts),
-  }):find()
+-- 
+-- search_notes:
+-- 
+-- find the file linked to by the word under the cursor
+-- 
+search_notes = function(opts)
+    builtin.live_grep({
+        prompt_title = "Search in notes",
+        cwd = zkcfg.home,
+        search_dirs = { zkcfg.home },
+        default_text = vim.fn.expand("<cword>"),
+        find_command = { zkcfg.daily_finder },
+   })
 end
 
 
---[[
+--[[ 
+-- interesting snippet:
     function file_exists(name)
        local f=io.open(name,"r")
        if f~=nil then io.close(f) return true else return false end
@@ -295,9 +197,9 @@ local M = {
     zkcfg = zkcfg,
     find_notes = find_notes,
     find_daily_notes = find_daily_notes,
+    search_notes = search_notes,
     insert_link = insert_link,
     follow_link = follow_link,
-    find_notes = find_filenames,
     setup = setup,
     install_daily_finder = install_daily_finder,
 }
