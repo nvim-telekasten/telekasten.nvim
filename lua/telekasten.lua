efc22677c6c1221af42db0ddfa7edab41afde6d3
Ernie Miller <ernie@ernie.io>
2022-05-09 09:34:30 -0400

    Try to get more consistent relative path behavior

--- lua/telekasten.lua
+++ lua/telekasten.lua
@@ -1269,6 +1269,7 @@ function picker_actions.paste_img_link(opts)
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
         local fn = selection.value
+        fn = fn:gsub(escape(M.Cfg.home .. "/"), "")
         fn = make_relative_path(vim.fn.expand("%"), fn, "/")
         local imglink = "![](" .. fn .. ")"
         vim.api.nvim_put({ imglink }, "", true, true)
@@ -1286,6 +1287,7 @@ function picker_actions.yank_img_link(opts)
         end
         local selection = action_state.get_selected_entry()
         local fn = selection.value
+        fn = fn:gsub(escape(M.Cfg.home .. "/"), "")
         fn = make_relative_path(vim.fn.expand("%"), fn, "/")
         local imglink = "![](" .. fn .. ")"
         vim.fn.setreg('"', imglink)
